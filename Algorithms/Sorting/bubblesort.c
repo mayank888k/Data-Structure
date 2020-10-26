891a0db62b18d9a738759f18473d78ae212eda3a
Mayank Kumar <mayank963k@gmail.com>
2020-10-26 17:06:43 +0530

    Bubble Sort

---
+++ Algorithms/Sorting/bubblesort.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void swap(int *a, int *b)
+{
+    int temp;
+    temp=*a;
+    *a=*b;
+    *b=temp;
+}
+
+void bubblesort(int *a,int n)
+{
+    for (int i = 0; i < n-1; i++)
+    {
+        for (int j = 0; j < n-i-1; j++)
+        {
+            if (a[j]>a[j+1])
+            {
+                swap(&a[j],&a[j+1]);
+            }
+            
+        }
+        
+    }
+    
+}
+int main()
+{
+    int arr[20],n;
+    printf("Enter the size of array :");
+    scanf("%d",&n);
+
+    printf("Enter the Elements of Array\n");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("\nArray Before Sorting\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d\t",arr[i]);
+    }
+    bubblesort(arr,n);
+    printf("\nArray After Sorting\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d\t",arr[i]);
+    }
+    return 0;
+    
+}
